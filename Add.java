46e509af27845311a62ec9bc85ce49c4120dfd4b
abhishekjoshi2003 <91530015+abhishekjoshi2003@users.noreply.github.com>
2023-05-09 03:10:29 -0700

    Addition program in java

---
+++ Add.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javalearning;
+
+/**
+ *
+ * @author RCAT
+ */
+public class Add {
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 21;
+        int c = a+ b;
+        System.out.println("Sum of a and b is :"+c);
+    }
+    
+}
